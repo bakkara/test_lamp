d6bf46a6a663ea07b694e58e67881dbd26279fbc
bakkara <ane4karymar@gmail.com>
2023-10-26 17:18:44 +0300

    add media

--- js/carousel.js
+++ js/carousel.js
@@ -3,9 +3,20 @@ $(document).ready(function () {
         loop:true,
         margin: 32,
         autoWidth:true,
-        items:4,
         autoplay: true,
         autoplayTimeout:1000,
         autoplayHoverPause: true,
+        responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:3,
+        },
+        1158:{
+            items:4,
+        }
+    }
     });
 })
